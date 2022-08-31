b1ef579dcda14bea81a47df2b9334ffe11a5e8c0
Deoroot <angelshss@gmail.com>
2022-08-30 19:46:51 -0500

    More Fixes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
